77c35aaa013c3cd751a2b4f752949992dfa90779
moerwald <andreas.mewald@gmail.com>
2020-04-21 09:51:45 +0200

    - Added new requires method.

--- src/Conditions/PreConditions.cs
+++ src/Conditions/PreConditions.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
 using System.Diagnostics;
 
-namespace Conditions.PreConditions
+namespace Mwd.Conditions
 {
     public static class PreCondition
     {
         public static void Requires<T>(this T obj, Func<bool> predicate, string description)
            => Requires<T>(predicate, description);
+        public static void RequiresNotNull<T>(this T obj, string description) where T : class
+           => Requires<T>(() => obj != null, description);
 
         public static void Requires<T>(Func<bool> predicate, string description)
         {
@@ -32,34 +33,5 @@ namespace Conditions.PreConditions
             string description)
             => 
             Requires<T>(() => enumerable.Any(predicate), description);
-    }
-
-    public static class PostCondition
-    {
-        private const string Dbg = "DEBUG";
-        [Conditional(Dbg)]
-        public static void Ensures<T>(this T obj, Func<bool> predicate, string description)
-            => Ensures(predicate, description);
-
-        [Conditional("DEBUG")]
-        public static void Ensures(Func<bool> predicate, string description)
-        {
-            if (predicate?.Invoke() ?? false) return;
-
-            Debug.Assert(false, description);
-        }
-
-        [Conditional("DEBUG")]
-        public static void EnsuresAll<T>(this IEnumerable<T> enumerable, Func<T,bool> predicate, string description)
-            => Ensures(() => enumerable.All(predicate), description);
-
-        [Conditional("DEBUG")]
-        public static void EnsuresAny<T>(this IEnumerable<T> enumerable, Func<T,bool> predicate, string description)
-            => Ensures(() => enumerable.Any(predicate), description);
-
-        [Conditional("DEBUG")]
-        public static void EnsuresSingle<T>(this IEnumerable<T> enumerable, Func<T,bool> predicate, string description)
-            => Ensures(() => enumerable.SingleOrDefault(predicate).Equals(default) == false , description);
-
     }
 }
